0729323e5bc499fc933bb45ecdf7ad190cb5ebc1
Dinh Thanh An <thanhan.uit@gmail.com>
2017-03-30 18:13:23 +0700

    first init

---
+++ SwiftPractice/ViewController/Controller/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  SwiftPractice
+//
+//  Created by Dinh Thanh An on 3/30/17.
+//  Copyright © 2017 Dinh Thanh An. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
